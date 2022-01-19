4058ad621d78b5c3c4878d0cb28b72bd5cebf3c0
fabiuz <ccmaismais@yahoo.com>
2022-01-19 18:57:41 -0300

    Alterando nome de "button1" pra "Desmarcar_Maiusculas".

--- Gerador_de_Palavras/Form1.Designer.cs
+++ Gerador_de_Palavras/Form1.Designer.cs
@@ -55,7 +55,7 @@
             this.btnDesmarcar_Maiuscula_az = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.lskMaiuscula = new System.Windows.Forms.CheckedListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnDesmarcar_Maiuscula = new System.Windows.Forms.Button();
             this.btnMarcar_Maiuscula = new System.Windows.Forms.Button();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.btnMarcar_Digitos = new System.Windows.Forms.Button();
@@ -371,10 +371,10 @@
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.Controls.Add(this.groupBox7, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnDesmarcar_Maiuscula, 1, 2);
             this.tableLayoutPanel3.Controls.Add(this.btnMarcar_Maiuscula_az, 0, 1);
             this.tableLayoutPanel3.Controls.Add(this.btnDesmarcar_Maiuscula_az, 0, 2);
             this.tableLayoutPanel3.Controls.Add(this.groupBox4, 1, 0);
-            this.tableLayoutPanel3.Controls.Add(this.button1, 1, 2);
             this.tableLayoutPanel3.Controls.Add(this.btnMarcar_Maiuscula, 1, 1);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
@@ -456,17 +456,17 @@
             this.lskMaiuscula.TabIndex = 0;
             this.lskMaiuscula.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkBox_ItemCheck);
             // 
-            // button1
+            // btnDesmarcar_Maiuscula
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.btnDesmarcar_Maiuscula.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(203, 285);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 22);
-            this.button1.TabIndex = 29;
-            this.button1.Text = "Desmarcar todos";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnDesmarcar_Maiuscula_Click);
+            this.btnDesmarcar_Maiuscula.Location = new System.Drawing.Point(203, 285);
+            this.btnDesmarcar_Maiuscula.Name = "btnDesmarcar_Maiuscula";
+            this.btnDesmarcar_Maiuscula.Size = new System.Drawing.Size(194, 22);
+            this.btnDesmarcar_Maiuscula.TabIndex = 29;
+            this.btnDesmarcar_Maiuscula.Text = "Desmarcar todos";
+            this.btnDesmarcar_Maiuscula.UseVisualStyleBackColor = true;
+            this.btnDesmarcar_Maiuscula.Click += new System.EventHandler(this.btnDesmarcar_Maiuscula_Click);
             // 
             // btnMarcar_Maiuscula
             // 
@@ -1005,7 +1005,7 @@
         private System.Windows.Forms.Button btnDesmarcar_Maiuscula_az;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.CheckedListBox lskMaiuscula;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDesmarcar_Maiuscula;
         private System.Windows.Forms.Button btnMarcar_Maiuscula;
         private System.Windows.Forms.TabPage tabPage6;
         private System.Windows.Forms.Button btnMarcar_Digitos;
